b5490a68a3e580f55f1364ac5222ae330bf475a0
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-08-18 22:53:55 -0300

    refactor(default-files): moved and renamed default react create files

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-import logo from '/wbd-2021.png';
-import './App.css';
+import './styles/globals.css';
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo} className="logo" alt="WimBelemDon logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    return <Home />;
 }
 
 export default App;
