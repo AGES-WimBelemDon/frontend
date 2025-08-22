14ffe7f22dbc0bfc66a652f7d24f0284ea173720
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-08-22 01:00:43 -0300

    Chore - fix pages basepath (#14)
    * fix(base-path): add /frontend to support github pages
    
    * chore(gitignore): include cypress screenshots folder
    
    * fix(cypress-env): updated values to match new base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/frontend/',
   plugins: [react(), VitePWA({
     devOptions: { enabled: true },
     manifest: {
