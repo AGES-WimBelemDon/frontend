c7e5acca57544b8f2ad3b4ca175403cc562079cc
Bernardo Lykawka <bernardolmsilvas@gmail.com>
2025-08-31 22:12:58 -0300

    feat(ui): add header (#19)
    * feat(ui): add header
    
    * fix(lint)
    
    ---------
    
    Co-authored-by: Bernardo Lykawka <bernardo.lykawka@edu.pucrs.br>

--- src/pages/TechDemo/index.tsx
+++ src/pages/TechDemo/index.tsx
@@ -2,6 +2,7 @@ import { Button, Container, Stack, Typography } from '@mui/material';
 import { Outlet, useNavigate } from 'react-router';
 
 import UserProfile from './Profile';
+import Header from '../../components/header';
 import { pt } from '../../constants';
 
 function TechDemo() {
@@ -11,21 +12,24 @@ function TechDemo() {
     navigate('2?c2=4');
   }
 
-  return (
-    <Container maxWidth="sm">
-      <Stack>
-        <Typography variant='h1' data-cy="tech-demo-title">{pt.techDemo.title}</Typography>
-        <Button
-          onClick={showAPIInfo}
-          variant="contained"
-          color="primary"
-          data-cy="tech-demo-show-api-info-button"
-        >
-          <Typography variant='body1'>{pt.techDemo.showAPIInfo}</Typography>
-        </Button>
-      </Stack>
-      <UserProfile />
-      <Outlet />
+  return ( 
+    <Container>
+      <Header/>
+      <Container maxWidth="sm">
+        <Stack>
+          <Typography variant='h1' data-cy="tech-demo-title">{pt.techDemo.title}</Typography>
+          <Button
+            onClick={showAPIInfo}
+            variant="contained"
+            color="primary"
+            data-cy="tech-demo-show-api-info-button"
+          >
+            <Typography variant='body1'>{pt.techDemo.showAPIInfo}</Typography>
+          </Button>
+        </Stack>
+        <UserProfile />
+        <Outlet />
+      </Container>
     </Container>
   );
 }
