cb2309eb7688dc5387c61f27cb4c97ef2b869c03
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-09-03 18:57:01 -0300

    Feature: Text Card Component (#21)
    Co-authored-by: Leonardo Mallet Scheidt <leonardo.mallet@edu.pucrs.br>

---
+++ src/components/TextCard/index.tsx
@@ -0,0 +1,38 @@
+import HomeIcon from '@mui/icons-material/Home';
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+  
+import type { ThemeStyle, TextCard } from './interface';
+
+const themeStyle: Record<ThemeStyle, object> = {
+  light: { backgroundColor: 'primary.main', color: 'secondary.main' },  
+  dark: { backgroundColor: 'secondary.main', color: 'primary.main' }, 
+};
+
+export function TextCard({ title, theme, onClick }: TextCard) {
+  return (
+    <Card
+      sx={{
+        ...themeStyle[theme],
+        borderWidth: 2,  
+        borderRadius: 2,  
+        borderStyle: 'solid',  
+        borderColor: 'secondary.main',  
+        height: 125,
+        flex: 1,
+        alignItems: 'center',
+      }}
+    >
+      <CardActionArea sx={{height: '100%'}} onClick={onClick}>
+        <CardContent >
+          <HomeIcon fontSize="large" />
+          <Typography
+            sx={{fontWeight: 'bold' }}
+            variant={'h5'}
+          >
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,27 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, type PaletteColor, type PaletteColorOptions } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    customColors: Record<string, PaletteColor>;
+  }
+  interface PaletteOptions {
+    customColors?: Record<string, PaletteColorOptions>;
+  }
+}
 
 export const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      light: '#387b81',
-      main: '#075b62',
-      dark: '#043f44',
+      main: '#FFFFFF',
     },
     secondary: {
-      light: '#a3d266',
-      main: '#8dc740',
-      dark: '#628b2c',
+      main: '#0E6872',
     },
+    customColors: {
+      terciary: {
+        main: '#8DC740'
+      }
+    }
   },
 });
