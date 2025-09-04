8fc24858e500476c00b33598fcd7618d87d30dee
Leonardo Mallet Scheidt <leonardo.mallet@edu.pucrs.br>
2025-09-04 17:34:03 -0300

    feat: Added the toast to the App Root and an test button in the TechDemo

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { AuthProvider } from './contexts/AuthProvider';
+import { Toast } from './components/Toast';
+import { AuthProvider } from './contexts/Auth/AuthProvider';
+import { ToastProvider } from './contexts/Toast/ToastProvider';
 import AppRouter from './routes/AppRouter';
 import { theme } from './styles/theme';
 
 const queryClient = new QueryClient();
 
 function App() {
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AuthProvider>
-          <AppRouter />
+          <ToastProvider>
+            <AppRouter />
+            <Toast></Toast>
+          </ToastProvider>
         </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
--- src/pages/TechDemo/index.tsx
+++ src/pages/TechDemo/index.tsx
@@ -4,27 +4,40 @@ import { Outlet, useNavigate } from 'react-router';
 import UserProfile from './Profile';
 import Header from '../../components/header';
 import { pt } from '../../constants';
+import { useToast } from '../../hooks/useToast';
 
 function TechDemo() {
   const navigate = useNavigate();
-  
+  const {showToast} = useToast();
+
   function showAPIInfo() {
     navigate('2?c2=4');
   }
 
-  return ( 
+  return (
     <Container>
-      <Header/>
+      <Header />
       <Container maxWidth="sm">
         <Stack>
-          <Typography variant='h1' data-cy="tech-demo-title">{pt.techDemo.title}</Typography>
+          <Typography variant="h1" data-cy="tech-demo-title">
+            {pt.techDemo.title}
+          </Typography>
           <Button
             onClick={showAPIInfo}
             variant="contained"
             color="primary"
             data-cy="tech-demo-show-api-info-button"
           >
-            <Typography variant='body1'>{pt.techDemo.showAPIInfo}</Typography>
+            <Typography variant="body1">{pt.techDemo.showAPIInfo}</Typography>
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            data-cy="tech-demo-show-api-info-button"
+            onClick={() => showToast('Teste de Toast', 'success') }
+          >
+            Abrir TOAST!
           </Button>
         </Stack>
         <UserProfile />
