11512ad0add3f9872a609bc83fe168ea25b29eb1
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-09-04 03:44:16 -0300

    refactor(ui-tech-demo): restructure layout with nicer separation

--- src/pages/TechDemo/index.tsx
+++ src/pages/TechDemo/index.tsx
@@ -1,8 +1,7 @@
 import HomeIcon from '@mui/icons-material/Home';
-import { Button, Container, Stack, Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import { Outlet, useNavigate } from 'react-router';
 
-import UserProfile from './Profile';
 import { Header } from '../../components/Header';
 import { TextCard } from '../../components/TextCard';
 import { pt } from '../../constants';
@@ -14,33 +13,41 @@ function TechDemo() {
     navigate('2?c2=4');
   }
 
-  return ( 
+  return (
     <>
-      <Header/>
+      <Header />
       <Container maxWidth="md">
-        <Stack>
-          <Typography variant='h1' data-cy="tech-demo-title">{pt.techDemo.title}</Typography>
-          <Button
-            onClick={showAPIInfo}
-            variant="contained"
-            color="primary"
-            data-cy="tech-demo-show-api-info-button"
-          >
-            <Typography variant='body1'>{pt.techDemo.showAPIInfo}</Typography>
-          </Button>
-        </Stack>
-        <TextCard
-          title="Home"
-          theme="dark"
-          icon={<HomeIcon />}
-          onClick={() => console.log('clicou') }
-        />
-        <TextCard
-          title="Home"
-          theme="light"
-          icon={<HomeIcon />}
-        />
-        <UserProfile />
+        <Typography variant='h1' data-cy="tech-demo-title">{pt.techDemo.title}</Typography>
+
+        <hr />
+
+        <Grid container spacing={2}>
+          <TextCard
+            title="Home"
+            theme="dark"
+            icon={<HomeIcon />}
+            onClick={() => console.log('clicou')}
+          />
+          <TextCard
+            title="Home"
+            theme="light"
+            icon={<HomeIcon />}
+          />
+        </Grid>
+
+        <hr />
+
+        <Button
+          onClick={showAPIInfo}
+          variant="contained"
+          color="primary"
+          data-cy="tech-demo-show-api-info-button"
+        >
+          <Typography variant='body1'>{pt.techDemo.showAPIInfo}</Typography>
+        </Button>
+
+        <hr />
+
         <Outlet />
       </Container>
     </>
