67a18d7a22a0bfd3e4a4df85b478b316fb467950
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-09-08 19:57:46 -0300

    refactor(card-list): support for two columns on medium sized screens

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -5,13 +5,25 @@ import { Grid, useMediaQuery, useTheme } from '@mui/material';
 export function CardList({ children }: PropsWithChildren) {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
+  const gridColumnsCountMapper = (() => {
+    switch (true) {
+    case isMobile:
+      return 1;
+    case isDesktop:
+      return 3;
+    default:
+      return 2;
+    }
+  })();
   
   return (
     <Grid
       container
       display='grid'
       spacing={{ xs: 1 }}
-      gridTemplateColumns={isMobile ? 'repeat(1, 1fr)' : 'repeat(3, 1fr)'}
+      gridTemplateColumns={`repeat(${gridColumnsCountMapper}, 1fr)`}
     >
       {children}
     </Grid>
