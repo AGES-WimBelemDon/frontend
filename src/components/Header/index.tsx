b7c0450cc49de7078d25226d14d8576948327210
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-09-07 22:31:56 -0300

    refactor(header): now has centralized text logo only

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,53 +1,48 @@
-import { Menu } from '@mui/icons-material';
-import { Box, ButtonBase, Divider, IconButton } from '@mui/material';
+import { Box, ButtonBase, Divider, Tooltip, Typography } from '@mui/material';
+import { useNavigate } from 'react-router';
 
-import { UserProfile } from './UserProfile';
 import { pt } from '../../constants';
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Box
         component="header"
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'auto 1fr auto',
+          display: 'flex',
+          justifyContent: 'center',
           px: { xs: 2, md: 2.5 },
           py: { xs: 2.5, md: 1.5 },
-          alignItems: 'center',
-          justifyItems: 'center',
         }}
       >
-        <IconButton
-          aria-label={pt.header.openSidebar}
-          data-cy="header-sidebar-button"
-          onClick={() => alert(pt.header.openSidebar)}
-        >
-          <Menu
-            sx={{
-              fontSize: { xs: 30, md: 40 },
-            }}
-            color="primary"
-          />
-        </IconButton>
-        <ButtonBase
-          sx={{ borderRadius: '50%' }}
-          focusRipple
-          aria-label={pt.header.goToHome}
-          data-cy="header-home-button"
-          onClick={() => alert(pt.header.goToHome)}
-        >
-          <Box
-            component="img"
-            src="./logo.png"
-            alt={pt.header.logoAlt}
-            sx={{
-              height: { xs: 45, md: 60 },
-              width: { xs: 45, md: 60 },
-            }}
-          />
-        </ButtonBase>
-        <UserProfile />
+        <Tooltip title={pt.header.goToHome} placement='right'>
+          <ButtonBase
+            focusRipple
+            aria-label={pt.header.goToHome}
+            data-cy="header-home-button"
+            onClick={() => navigate('/')}
+            sx={{ gap: 2, display: 'flex', paddingX: 6 }}
+          >
+            <Box
+              component="img"
+              src="./logo.png"
+              alt={pt.header.logoAlt}
+              sx={{
+                height: { xs: 45, md: 60 },
+                width: { xs: 45, md: 60 },
+              }}
+            />
+            <Typography
+              variant="h1"
+              fontSize={{ xs: 20, md: 30 }}
+              fontWeight='bold'
+            >
+              WimBelemDon+
+            </Typography>
+          </ButtonBase>
+        </Tooltip>
       </Box>
       <Divider
         variant="middle"
