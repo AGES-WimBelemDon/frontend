477d928936e32ab1f7fac59f22b5cc60c0b4efd0
Felipe Freitas <f.freitas007@edu.pucrs.br>
2025-09-10 17:11:35 -0300

    Revert "chore(eslint): applied linting rules"
    This reverts commit a66b5f7b34860d4448cddbf48721cef202a3ed0a.

--- cypress/e2e/tech-demo.cy.js
+++ cypress/e2e/tech-demo.cy.js
@@ -1,41 +1,41 @@
-describe("tech demo test", () => {
+describe('tech demo test', () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.dataCy("tech-demo-nav-link").click();
+    cy.visit('/');
+    cy.dataCy('tech-demo-nav-link').click();
   });
 
-  it("navigates to the tech demo page", () => {
-    cy.url().should("include", "/tech-demo");
+  it('navigates to the tech demo page', () => {
+    cy.url().should('include', '/tech-demo');
   });
 
-  it("renders the title", () => {
-    cy.dataCy("tech-demo-title").should("have.text", "Tech Demo");
+  it('renders the title', () => {
+    cy.dataCy('tech-demo-title').should('have.text', 'Tech Demo');
   });
 
-  it("renders button to show API info", () => {
-    cy.dataCy("tech-demo-show-api-info-button").should("exist");
+  it('renders button to show API info', () => {
+    cy.dataCy('tech-demo-show-api-info-button').should('exist');
   });
 
-  it("navigates when clicking show API info button", () => {
-    cy.dataCy("tech-demo-show-api-info-button").click();
-    cy.url().should("include", "/2?c2=4");
+  it('navigates when clicking show API info button', () => {
+    cy.dataCy('tech-demo-show-api-info-button').click();
+    cy.url().should('include', '/2?c2=4');
   });
 });
 
-describe("tech demo API info", () => {
+describe('tech demo API info', () => {
   beforeEach(() => {
-    cy.interceptOrMock("getTechDemo", "GET", "**/repos/tanstack/query", "tech-demo.json");
+    cy.interceptOrMock('getTechDemo', 'GET', '**/repos/tanstack/query', 'tech-demo.json');
 
-    cy.visit("/tech-demo");
-    cy.dataCy("tech-demo-show-api-info-button").click();
+    cy.visit('/tech-demo');
+    cy.dataCy('tech-demo-show-api-info-button').click();
   });
 
-  it("fetches and displays API info", () => {
-    cy.wait("@getTechDemo", { timeout: 10_000 });
-    cy.dataCy("tech-demo-api-full-name").should("have.text", "TanStack/query");
-    cy.dataCy("tech-demo-api-description").should("include.text", "🤖 Powerful asynchronous state management, server-state utilities and data fetching for the web. TS/JS, React Query, Solid Query, Svelte Query and Vue Query.");
-    cy.dataCy("tech-demo-api-subscribers-count").should("include.text", "214");
-    cy.dataCy("tech-demo-api-stargazers-count").should("include.text", "46314");
-    cy.dataCy("tech-demo-api-forks-count").should("include.text", "3410");
+  it('fetches and displays API info', () => {
+    cy.wait('@getTechDemo', { timeout: 10_000 });
+    cy.dataCy('tech-demo-api-full-name').should('have.text', 'TanStack/query');
+    cy.dataCy('tech-demo-api-description').should('include.text', '🤖 Powerful asynchronous state management, server-state utilities and data fetching for the web. TS/JS, React Query, Solid Query, Svelte Query and Vue Query.');
+    cy.dataCy('tech-demo-api-subscribers-count').should('include.text', '214');
+    cy.dataCy('tech-demo-api-stargazers-count').should('include.text', '46314');
+    cy.dataCy('tech-demo-api-forks-count').should('include.text', '3410');
   });
 });
--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
-import js from "@eslint/js";
-import { globalIgnores } from "eslint/config";
-import importPlugin from "eslint-plugin-import";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-  globalIgnores(["dist", "dev-dist"]),
+  globalIgnores(['dist', 'dev-dist']),
   {
-    files: ["**/*.{ts,tsx}", "**/*.{mjs,js}"],
+    files: ['**/*.{ts,tsx}', '**/*.{mjs,js}'],
     plugins: {
       import: importPlugin,
-      "jsx-a11y": jsxA11y,
+      'jsx-a11y': jsxA11y,
     },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -27,26 +27,26 @@ export default tseslint.config([
     },
     rules: {
       ...jsxA11y.configs.strict.rules,
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
           groups: [
-            ["builtin", "external"],
-            ["internal", "parent", "sibling"],
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling'],
           ],
           pathGroups: [{
-            pattern: "react",
-            group: "external",
-            position: "before"
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
           }],
-          pathGroupsExcludedImportTypes: ["react"],
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true }
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true }
         }
       ],
-      "indent": ["error", 2],
-      "quotes": ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
-      "jsx-quotes": ["error", "prefer-double"]
+      'indent': ['error', 2],
+      'quotes': ['error', 'double', { avoidEscape: true, allowTemplateLiterals: true }],
+      'jsx-quotes': ['error', 'prefer-double']
     }
   },
 ]);
