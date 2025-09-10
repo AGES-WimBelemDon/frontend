0ef21f681bd903700c0af39ea8708f156375aeba
Leonardo Gemin <l.gemin@edu.pucrs.br>
2025-09-10 19:55:07 -0300

    feat(ui): title responsivity

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,11 +1,13 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery } from '@mui/material';
 
 import type { PageTitleProps } from './interface';
 
 export function PageTitle({title}: PageTitleProps) {
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
   return (
     <Box paddingY={2.5} paddingX={0.5}>
-      <Typography variant='h1' fontWeight={'bold'} fontSize={24}>
+      <Typography variant='h1' fontWeight={'bold'} fontSize={isMobile ? 24 : 30}>
         {title}
       </Typography>
     </Box>
