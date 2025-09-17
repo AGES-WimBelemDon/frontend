ce2246d129b3d60300c76475878e70bdff1ff810
Leonardo Mallet Scheidt <leonardo.mallet@edu.pucrs.br>
2025-09-17 20:00:33 -0300

    Refact: Husky corrections

--- src/components/PersonCard/index.tsx
+++ src/components/PersonCard/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import EditIcon from "@mui/icons-material/Edit";
-import { Box, Button, Card, CardMedia, Typography } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { Box, Button, Card, CardMedia, Typography } from '@mui/material';
 
-import type { PersonCardData } from "./interface";
+import type { PersonCardData } from './interface';
 
 export function PersonCard({
   name,
@@ -32,35 +32,35 @@ export function PersonCard({
   return (
     <Card
       sx={{
-        display: "flex",
-        flexDirection: "row",
-        height: "9vh",
-        width: "100% ",
-        backgroundColor: "background.default",
+        display: 'flex',
+        flexDirection: 'row',
+        height: '9vh',
+        width: '100% ',
+        backgroundColor: 'background.default',
       }}
     >
       <CardMedia
         component="img"
-        sx={{ width: "9vh ", height: "100%", padding: 1.2, borderRadius: 3 }}
+        sx={{ width: '9vh ', height: '100%', padding: 1.2, borderRadius: 3 }}
         image="public/userImg.png"
         alt="Ícone"
       />
-      <Box sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
+      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row' }}>
         <Typography component="span" sx={{ padding: 1.6 }}>
-          <strong>Nome:</strong> {cardData.name} <strong>CPF:</strong>{" "}
-          {cardData.cpf} <strong>Nascimento:</strong> {cardData.birthDate}{" "}
-          <strong>Estado Civil: </strong> {cardData.civilState}{" "}
-          <strong>NIS: </strong> {cardData.nis} <strong>Telefone: </strong>{" "}
-          {cardData.phone} <strong>Email: </strong> {cardData.email}{" "}
-          <strong>Endereço: </strong> {cardData.address}{" "}
+          <strong>Nome:</strong> {cardData.name} <strong>CPF:</strong>{' '}
+          {cardData.cpf} <strong>Nascimento:</strong> {cardData.birthDate}{' '}
+          <strong>Estado Civil: </strong> {cardData.civilState}{' '}
+          <strong>NIS: </strong> {cardData.nis} <strong>Telefone: </strong>{' '}
+          {cardData.phone} <strong>Email: </strong> {cardData.email}{' '}
+          <strong>Endereço: </strong> {cardData.address}{' '}
         </Typography>
       </Box>
 
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "end",
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'end',
           p: 1.5,
         }}
       >
@@ -69,26 +69,26 @@ export function PersonCard({
           variant="contained"
           onClick={() =>
             onEdit({
-              name: "New name",
-              cpf: "New cpf",
-              birthDate: "new date",
-              civilState: "new state",
-              nis: "new nis",
-              phone: "New Phone",
-              email: "New email",
-              address: "New Address",
+              name: 'New name',
+              cpf: 'New cpf',
+              birthDate: 'new date',
+              civilState: 'new state',
+              nis: 'new nis',
+              phone: 'New Phone',
+              email: 'New email',
+              address: 'New Address',
             })
           }
           endIcon={<EditIcon fontSize="small" />}
           sx={{
-            textTransform: "none",
-            height: "1.5rem",
+            textTransform: 'none',
+            height: '1.5rem',
             borderRadius: 4,
             px: 1.5,
             py: 0.4,
             fontWeight: 700,
-            bgcolor: "wbTeal",
-            ":hover": { bgcolor: "#0f5d5d" },
+            bgcolor: 'wbTeal',
+            ':hover': { bgcolor: '#0f5d5d' },
           }}
         >
           Editar
