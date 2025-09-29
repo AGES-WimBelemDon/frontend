1af8e76e2900f03f0f0ee86c5e6dd7e761c22e2a
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-09-28 23:53:16 -0300

    feat(student-registration): fetch address details on external url

--- src/services/address.ts
+++ src/services/address.ts
@@ -4,3 +4,23 @@ export type Address = {
   number: string;
   complement?: string;
 }
+
+export async function fetchAddress(cep: string): Promise<Partial<Address> | null> {
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    const street = [data.logradouro, data.complemento, data.bairro, data.localidade, data.uf]
+    
+    return {
+      street: street.filter(Boolean).join(", "),
+    };
+  } catch (error) {
+    console.error("Error fetching address:", error);
+    return null;
+  }
+}
