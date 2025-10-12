2fd19fbf3764583ef0c68af01f90df92a9a833a6
Renatu5 <renato.souza003@edu.pucrs.br>
2025-10-12 17:55:54 -0300

    feat(ClassesModal): adjustment fields and inserting date bullet - @Renatu5

--- src/components/ClassesModal/index.tsx
+++ src/components/ClassesModal/index.tsx
@@ -1,36 +1,65 @@
 import React from "react";
 
+import { RadioButtonChecked, RadioButtonUnchecked } from "@mui/icons-material";
 import CloseIcon from "@mui/icons-material/Close";
-import { Box, Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, IconButton, InputLabel, ListItemText, MenuItem, Select, Switch, type SelectChangeEvent } from "@mui/material";
+import { Box, Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, FormGroup, IconButton, InputLabel, ListItemText, MenuItem, Radio, RadioGroup, Select, type SelectChangeEvent } from "@mui/material";
 
 import { useClassesModal } from "./hook";
-import { TextInput } from "../Inputs/TextInput";
+import { theme } from "../../styles/theme";
 export function ClassesModal() {
   const { isOpen, closeModal, createClass } = useClassesModal();
   const [personName, setPersonName] = React.useState<string[]>([]);
+  const isChecked = false;
 
-  const data = ["segunda", "terça", "quarta", "quinta", "sexta"];
+  const days = ["S", "T", "Q", "Q", "S", "S", "D"];
+  const level = ["Iniciante", "Intermediário", "Avançado"];
+  const professores = ["profeessora A", "Professo B", "Professora C", "Professor D"];
+  const alunos = ["Aluna A", "Aluno B", "Aluna C", "Aluno D"];
 
   const handleChange = (event: SelectChangeEvent<typeof personName>) => {
     const {
       target: { value },
     } = event;
     setPersonName(
-      // On autofill we get a stringified value.
       typeof value === "string" ? value.split(",") : value,
     );
   };
 
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
-  };
+  const DaysCalendarIcon = ({ text, checked }: { text: string, checked: boolean }) => {
+    const style = {
+      width: 22,
+      height: 22,
+      borderRadius: 5,
+      bgcolor: "primary.main",
+      color: "primary.contrastText",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: 18,
+      fontWeight: "semi-bold",
+    }
+    return (
+      checked ?
+        <Box
+          sx={{
+            ...style,
+          }}
+        >
+          {text}
+        </Box>
+        :
+        <Box
+          sx={{
+            ...style,
+            bgcolor: "background.paper",
+            color: "text.primary",
+          }}
+        >
+          {text}
+        </Box>
+    );
+  }
+
 
   return (
     <Dialog
@@ -88,29 +117,58 @@ export function ClassesModal() {
             display="flex"
             flexDirection="column"
           >
-            <FormControlLabel control={<Switch defaultChecked />} label="Ativar/Desativar Turma" />
-            <TextInput
-              label={"Teste"}
-              placeholder={"Testando input"}
-              id="1"
-            />
-            <FormControl sx={{ m: 2, width: 300 }}>
-              <InputLabel > Dia das turma</InputLabel>
-              <Select multiple value={personName} onChange={handleChange} MenuProps={MenuProps}>
-                {data.map((name) => (
-                  <MenuItem key={name} value={name} sx={{
-                    flexDirection: "row"
-                  }}>
-                    <Checkbox checked={personName.includes(name)} />
-                    <ListItemText primary={name} />
-                  </MenuItem>
-                ))}
-              </Select>
+
+            <InputLabel sx={{ color: theme.palette.text.primary }} id="classes-level">Nível das Turmas</InputLabel>
+
+            <Select sx={{}} value={personName} onChange={handleChange} labelId="classes-level">
+              {level.map((name) => (
+                <MenuItem key={name} value={name} sx={{
+                  flexDirection: "row",
+                  bgcolor: theme.palette.common.white,
+                  color: theme.palette.text.primary,
+                  "&:hover": {
+                    bgcolor: theme.palette.action.hover
+                  }
+                }}>
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+            <FormControlLabel control={<Checkbox size="small" sx={{ color: theme.palette.primary.main }}
+              icon={<RadioButtonUnchecked />}
+              checkedIcon={<RadioButtonChecked />}
+            />}
+            label="Turma Ativa" />
+            <InputLabel sx={{ color: theme.palette.text.primary }} id="classes-level">Dia da Semana</InputLabel>
+            <FormGroup sx={{ flexDirection: "row" }}>
+              {days.map((day, index) =>
+                <Checkbox key={`${day}` + `${index}`}
+                  checkedIcon={<DaysCalendarIcon text={day} checked={!isChecked} />}
+                  icon={<DaysCalendarIcon text={day} checked={isChecked} />}
+                  sx={{ color: theme.palette.primary.main }} />
+              )
+              }
+            </FormGroup>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                name="radio-buttons-group"
+              >
+                {professores.map((professor) =>
+                  <FormControlLabel key={professor} value={professor} control={<Radio sx={{ color: theme.palette.primary.main }} />} label={professor} />
+
+                )}
+              </RadioGroup>
             </FormControl>
+            Time
+            <FormGroup>
+              {alunos.map((aluno) =>
+                < FormControlLabel key={aluno} value={aluno} control={< Radio />} label={aluno} />
+              )}
+            </FormGroup>
           </Box>
         </Box>
       </DialogContent>
-
       <DialogActions>
         <Button variant="contained" onClick={createClass}>
           {"button de turma"}
