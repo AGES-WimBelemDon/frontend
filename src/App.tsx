b8286b1853d739d6d8e9ccbb78ab53ea18e6d994
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-10-27 00:39:50 -0300

    refactor: update LocalizationProvider to use dynamic locale from getUserLocale

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { getUserLocale } from "./constants";
 import { AuthProvider } from "./contexts/Auth/AuthProvider";
 import { SidebarProvider } from "./contexts/Sidebar/SidebarProvider";
 import { ToastProvider } from "./contexts/Toast/ToastProvider";
@@ -13,9 +14,11 @@ import { theme } from "./styles/theme";
 const queryClient = new QueryClient();
 
 export default function App() {
+  const preferedLocale = getUserLocale().toLocaleLowerCase();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={preferedLocale}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <AuthProvider>
