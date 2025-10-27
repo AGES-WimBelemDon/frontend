d7fef530c8216524e069ae2503a8738652ae395f
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2025-10-27 01:13:13 -0300

    fix: reorder routes in AppRouter for better organization

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import ActivityList from "../pages/Activity";
-import AnamnesisForm from "../pages/Anamnese/Form/index.tsx";
-import SelectForm from "../pages/Anamnese/SelectForm/";
+import AnamnesisForm from "../pages/Anamnese/Form";
+import SelectForm from "../pages/Anamnese/SelectForm";
 import Classes from "../pages/Classes";
 import Frequency from "../pages/Frequency";
 import { FrequencyCall } from "../pages/FrequencyCall";
@@ -10,11 +10,11 @@ import FrequencyClasses from "../pages/FrequencyClasses";
 import Home from "../pages/Home";
 import Layout from "../pages/Layout";
 import ResponsibleRegistration from "../pages/ResponsibleRegistration";
-import StudentEdition from "../pages/StudentEdition/index.tsx";
+import StudentEdition from "../pages/StudentEdition";
 import StudentRegistration from "../pages/StudentRegistration";
 import Students from "../pages/Students";
 import TechDemo from "../pages/TechDemo";
-import TechDemoMockAPI from "../pages/TechDemo/MockAPI.tsx";
+import TechDemoMockAPI from "../pages/TechDemo/MockAPI";
 import Users from "../pages/Users";
 
 function AppRouter() {
@@ -25,20 +25,14 @@ function AppRouter() {
           <Route index element={<Home />} />
           <Route path="alunos" element={<Students />} />
           <Route path="alunos/cadastro" element={<StudentRegistration />} />
+          <Route path="alunos/:id/editar" element={<StudentEdition />} />
           <Route path="alunos/:id/responsaveis" element={<ResponsibleRegistration />} />
-          <Route path="atividades" element={<ActivityList />} />
           <Route path="anamnese" element={<SelectForm />} />
           <Route path="anamnese/form/:id" element={<AnamnesisForm />} />
-          <Route path="alunos/:id/editar" element={<StudentEdition />} />
+          <Route path="atividades" element={<ActivityList />} />
           <Route path="frequencias/atividades" element={<Frequency />} />
-          <Route
-            path="frequencias/atividades/:id/turmas"
-            element={<FrequencyClasses />}
-          />
-          <Route
-            path="frequencias/atividades/:id/turmas/:id/chamada"
-            element={<FrequencyCall />}
-          />
+          <Route path="frequencias/atividades/:id/turmas" element={<FrequencyClasses />} />
+          <Route path="frequencias/atividades/:id/turmas/:id/chamada" element={<FrequencyCall />} />
           <Route path="tech-demo" element={<TechDemo />}>
             <Route path=":id" element={<TechDemoMockAPI />} />
           </Route>
