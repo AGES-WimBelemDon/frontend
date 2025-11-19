7bfdef8c6d2eab67828c2826c31ada6c1b62b00a
leogemin <leonardo.gemin.pereira@gmail.com>
2025-11-19 01:48:21 -0300

    fix: anamnesis form textField inputLabel

--- src/pages/Anamnese/Form/index.tsx
+++ src/pages/Anamnese/Form/index.tsx
@@ -5,6 +5,7 @@ import { useAnamneseForm } from "./useAnamneseForm";
 import { CardList } from "../../../components/CardList";
 import { PageTitle } from "../../../components/PageTitle";
 import { strings } from "../../../constants";
+import { useScreenSize } from "../../../hooks/useScreenSize";
 
 const AnamnesisForm = () => {
   const {
@@ -19,6 +20,8 @@ const AnamnesisForm = () => {
     handleFormChange,
   } = useAnamneseForm();
 
+  const { isMobile } = useScreenSize()
+
   return (
     <>
       <PageTitle title={strings.anamnesis.title} dataCy="anamnesis-form" />
@@ -26,9 +29,9 @@ const AnamnesisForm = () => {
         gap={2}
         display="flex"
         alignItems="center"
-        justifyContent="end"
-        marginBottom={3}
+        pb={3}
       >
+        <Button variant="outlined" onClick={handleCreateNew} fullWidth={isMobile}>{strings.anamnesis.createNew}</Button>
         {forms.length > 0 && (
           <FormControl variant="standard" sx={{ minWidth: 150 }}>
             <InputLabel>{strings.anamnesis.previousForms}</InputLabel>
@@ -41,21 +44,26 @@ const AnamnesisForm = () => {
             </Select>
           </FormControl>
         )}
-        <Button variant="outlined" onClick={handleCreateNew}>{strings.anamnesis.createNew}</Button>
       </Box>
 
       {(formId || isCreating) ? (
         <Box component="form" onSubmit={handleSubmit}>
           <CardList rowGap={5} columnGap={5}>
             {questions.map((question) => (
-              <TextField
-                key={question.id}
-                id={question.id}
-                label={question.statement}
-                variant="standard"
-                value={responses[question.id] || ""}
-                onChange={(e) => handleResponseChange(question.id, e.target.value)}
-              />
+              <Box display={"flex"} flexDirection={"column"} justifyContent={"space-between"}>
+                <InputLabel sx={{ color: "primary.main", textWrap: "wrap" }}>{question.statement}</InputLabel>
+                <TextField
+                  // slotProps={{
+                  //   inputLabel: { sx: { color: "primary.main" }, shrink: true },
+                  // }}
+                  key={question.id}
+                  id={question.id}
+                  variant="standard"
+                  value={responses[question.id] || ""}
+                  onChange={(e) => handleResponseChange(question.id, e.target.value)}
+                  fullWidth
+                />
+              </Box>
             ))}
           </CardList>
           <Box
